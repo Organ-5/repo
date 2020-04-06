e4acf81c6db55f64c7f1a8ac1ba80df71abf1a57
Winter-Cap <894197051@qq.com>
2020-04-06 19:30:44 +0800

    Update sin1.m

--- sin1.m
+++ sin1.m
@@ -1,3 +1,28 @@
+function y=nest(d,c,x,b)
+if nargin<4
+    b=zeros(d,1);
+end
+y=c(d+1);
+for i=d:-1:1
+    y=y.*(x-b(i))+c(i);
+end
+
+function c=newtdd(x,y,n)
+
+for j=1:n
+    v(j,1)=y(j);  %y column
+end
+
+for i=2:n  %column
+    for j=1:n+1-i   %row
+        v(j,i)=(v(j+1,i-1)-v(j,i-1))/(x(j+i-1)-x(j)); %count
+    end
+end
+
+for i=1:n
+    c(i)=v(1,i); %output the coefficient
+end
+
 %create sin value
 %input:x
 %output:vagued sin(x)
